cf409fecf97a162705481368b547f3a32a021330
sdxcreative <foodiversity.usa@sodexo.com>
2011-04-30 17:26:02 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'eabd7b84d61520e17f0ccd1080624e73706eac2f169fe96877a633ae5b518340992bc7c808b1894435a79dec54e572d876859c0d905eede4db28d9f485cbef64'
